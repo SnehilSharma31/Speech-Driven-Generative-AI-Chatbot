8d04e377cc4497a6fca654074dd808e55fa72b62
Snehil Sharma <99540137+SnehilSharma31@users.noreply.github.com>
2023-08-22 19:05:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template, request, jsonify
 from chat import get_response
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route("/predict", methods=["POST"])
+@app.get("/")
+def index_get():
+    return render_template("base.html")
+
+@app.post("/predict")
 def predict():
-    data = request.get_json()
-    message = data.get("message")
-    response = get_response(message)
-    return jsonify({"answer": response})
+    text=request.get_json().get("message")
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
 
 if __name__ == "__main__":
     app.run(debug=False,host='0.0.0.0')
